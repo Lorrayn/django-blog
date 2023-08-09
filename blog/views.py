750bebd1ac390b49b1c10fa3f3b408ec5b46a5c8
LORRAYNE <o.miranda@aluno.ifsp.edu.br>
2023-08-09 11:04:37 -0300

    0908

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from blog.models import Post 
 def index(request):
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
 def ola(request):
-    #return HttpResponse('Olá Django')#
-    return render(request, 'home.html')
-
-# Create your views here.
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+    # Create your views here.
